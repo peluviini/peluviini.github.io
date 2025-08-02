c694127f8157666292ab7c7330d0fac7a1cbbc48
peluviini <aptenodytesforsteri2020@au.com>
2025-08-02 13:26:43 +0900

    Add: docs and shell script

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+
+cp -rf ./backend/page/ ./docs
---
+++ docs/page/frontend_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h7bdaee15375747e6: (a: number, b: number, c: number) => void;
+export const closure4547_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9961a5cb8a8e66ed: (a: number, b: number) => void;
+export const closure4550_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h576c425bb6ebe430: (a: number, b: number) => void;
+export const closure118882_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
